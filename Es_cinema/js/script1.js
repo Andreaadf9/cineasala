127dd32d91efd2c5e845b9a678d62ff568961da7
ric1101 <161829770+ric1101@users.noreply.github.com>
2024-04-26 15:18:56 +0200

    aggiunta funzione stampaFilm

--- Es_cinema/js/script1.js
+++ Es_cinema/js/script1.js
@@ -8,29 +8,56 @@ let arrayCarosello = ["The game", "The wolf of wall street", "Avatar"];
 const APIKEY = "4214e970";
 const ENDPOINT = "https://www.omdbapi.com/?";
 
-function carosello(titolo) {
-    fetch(`${ENDPOINT}t=${titolo}&apikey=${APIKEY}`)
-    .then(data => {
-        return data.json();
-    })
-    .then(response => {
-        return response;
-    })
+async function carosello(titolo) {
+    // await permette di attendere la fine dell'esecuzione di un istruzione. Altrimenti senza non finirebbe l'operazione e sarebbe undefined.
+    // Il for non riuscirebbe a caricare i dati in tempo dalla fetch. è come se da una chiamata asincrona passasse a sincrona.
+    let response = await fetch(`${ENDPOINT}t=${titolo}&apikey=${APIKEY}`);
+        return response.json();
 }
 
 
-function caricamentoDatiCarosello() {
+async function caricamentoDatiCarosello() {
     for (let i = 0; i < arrayCarosello.length; i++) {
         let titoloFilm = arrayCarosello[i];
-        carosello(titoloFilm)
-        .then(infoFilm => {
-            console.log(infoFilm); // Dati del film
-            arrayFilm.push(infoFilm); // Aggiungi il film all'array
-            console.log(arrayFilm); // Array con i film aggiunti
-        })
+        let infoFilm = await carosello(titoloFilm);
+        arrayFilm.push(infoFilm);
+        console.log(arrayFilm);
+        
     }
+    stampaFilm();
 }
 caricamentoDatiCarosello()
+
+function stampaFilm() {
+    arrayFilm.forEach(film => {
+        let caroselloFilm = `<div class="film">
+
+        <h2>${film.Title}</h2>
+
+        <div class="sliderCarosello">
+            <i class="fa-solid fa-angle-left"></i>
+            <div class="locandinaCarosello">
+                <img src="${film.Poster}" alt="">
+            </div>
+            <i class="fa-solid fa-angle-right"></i>
+        </div>
+
+        <div class="attori">
+            <a href="">${film.Actors}</a>
+        </div>
+
+        <div class="infoFilm">
+            <p>Durata <span>${film.Runtime}</span></p>
+            <p>Anno Uscita <span>${film.Year}</span></p>
+            <p>Genere <span>${film.Genre}</span></p>
+        </div>
+
+    </div>`;
+
+    carousel.innerHTML += caroselloFilm;
+
+    });
+}
 
 
 function avanti() {
@@ -51,3 +78,5 @@ btnCerca.addEventListener('click', cercaFilm);
 
 
 
+{/* <a href="">${film.Poster}</a>
+<a href="">${film.Poster}</a> */}
